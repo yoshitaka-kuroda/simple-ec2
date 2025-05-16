0db87259341abb79f01ba12ccd6ab799f2e85fe2
Your Name <you@example.com>
2025-05-16 11:30:03 +0900

    clean init: remove provider binaries

---
+++ main.tf
@@ -0,0 +1,85 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "ami-0c1638aa346a43fe8" # Amazon Linux 2023 (東京)
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+
+  tags = {
+    Name = "simple-web"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "Hello from Terraform!" > /var/www/html/index.html
+EOF
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "simple-ec2-key"
+  public_key = file("~/.ssh/simple-ec2-key.pub")
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "key_name" {
+  description = "EC2キーペア名（事前に作成済みのもの）"
+  type        = string
+}
